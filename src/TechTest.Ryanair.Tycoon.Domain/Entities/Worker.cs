729c064839cf162c0dfc1d5f1913a0db937e2ead
Brenon Ortega <brenon.ortega@hotmail.com>
2022-08-05 18:00:09 -0300

    starts creating worker

--- src/TechTest.Ryanair.Tycoon.Domain/Entities/Worker.cs
+++ src/TechTest.Ryanair.Tycoon.Domain/Entities/Worker.cs
@@ -1,6 +1,22 @@
+using TechTest.Ryanair.Tycoon.Domain.FluentApi.Activity;
+
 namespace TechTest.Ryanair.Tycoon.Domain.Entities;
 
 public class Worker : IActivityWorker
 {
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public List<Activity> Activities { get; set; }
+    public WorkerStatus Status { get; set; }
 
+    public WorksInResult WorksIn(Activity activity)
+    {
+        Activities.Any(x => x.StartDate <= activity )
+    }
 }
+    public enum WorkerStatus
+    {
+        Recharging,
+        Working,
+        Idle
+    }
