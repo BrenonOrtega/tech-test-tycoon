6a7f83620544cb208bd5b9866c5ac7d4e517f074
BrenonOrtega <bcosta@bancomaster.com.br>
2022-08-07 07:40:24 -0300

    Adds validation interface for commands

---
+++ src/TechTest.Ryanair.Tycoon.Application/UseCase/ICommand.cs
@@ -0,0 +1,14 @@
+﻿using Awarean.Sdk.Result;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechTest.Ryanair.Tycoon.Application.UseCase
+{
+    public interface ICommand
+    {
+        Result Validate();
+    }
+}
