cd8b2f4a9cd5a213fec1a508a011271e5388e067
BrenonOrtega <bcosta@bancomaster.com.br>
2022-08-07 07:42:52 -0300

    wip: starts adding tests for Workers with activities.

---
+++ tests/TechTest.Ryanair.Tycoon.UnitTests/Application/CreateWorkerWithActivitiesTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechTest.Ryanair.Tycoon.UnitTests.Application
+{
+    public class CreateWorkerWithActivitiesTests
+    {
+    }
+}
