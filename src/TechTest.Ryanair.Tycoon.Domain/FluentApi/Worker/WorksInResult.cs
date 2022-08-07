6e524ef11076e80be38ae8770f2530598f25cab5
Brenon Ortega <brenon.ortega@hotmail.com>
2022-08-06 23:30:27 -0300

    style fix in WorkIn Result

--- src/TechTest.Ryanair.Tycoon.Domain/FluentApi/Worker/WorksInResult.cs
+++ src/TechTest.Ryanair.Tycoon.Domain/FluentApi/Worker/WorksInResult.cs
@@ -1,22 +1,21 @@
 using Awarean.Sdk.Result;
 using TechTest.Ryanair.Tycoon.Domain.Entities;
 
-namespace TechTest.Ryanair.Tycoon.Domain.FluentApi.WorkerFluent
+namespace TechTest.Ryanair.Tycoon.Domain.FluentApi.WorkerFluent;
+
+public class WorksInResult : Result<Worker>, IActivityWorker
 {
-    public class WorksInResult : Result<Worker>, IActivityWorker
+    public TimedActivity FailedActivity { get; }
+    public WorksInResult(Worker worker) : base(worker) { }
+
+    public WorksInResult(Error error, TimedActivity failedActivity) : base(error)
+        => FailedActivity = failedActivity ?? throw new ArgumentNullException(nameof(failedActivity));
+
+    public WorksInResult WorksIn(TimedActivity activity)
     {
-        public TimedActivity FailedActivity { get; }
-        public WorksInResult(Worker worker) : base(worker) { }
-
-        public WorksInResult(Error error, TimedActivity failedActivity) : base(error)
-            => FailedActivity = failedActivity ?? throw new ArgumentNullException(nameof(failedActivity));
-
-        public WorksInResult WorksIn(TimedActivity activity)
-        {
-            if(IsFailed)
-                return this;
-            
-            return Value.WorksIn(activity);
-        }
+        if(IsFailed)
+            return this;
+        
+        return Value.WorksIn(activity);
     }
 }
