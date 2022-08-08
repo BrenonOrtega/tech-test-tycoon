6c045fb6636feb607be90e9e18410c06e78c3122
Brenon Ortega <brenon.ortega@hotmail.com>
2022-08-08 05:35:50 -0300

    adds tests for edge cases

--- tests/TechTest.Ryanair.Tycoon.UnitTests/Application/ScheduleActivityUseCaseTests.cs
+++ tests/TechTest.Ryanair.Tycoon.UnitTests/Application/ScheduleActivityUseCaseTests.cs
@@ -10,6 +10,31 @@ namespace TechTest.Ryanair.Tycoon.UnitTests.Application;
 public class ScheduleActivityUseCaseTests
 {
     private readonly IUnitOfWork unitOfWork = Substitute.For<IUnitOfWork>();
+
+    [Fact]
+    public async Task Creating_Activity_That_Workers_Can_Do_Should_Be_Success()
+    {
+        // Given
+        var sut = GetMockedUseCase();
+
+        var workers = new List<Worker> { new(Guid.NewGuid(), "A"), new(Guid.NewGuid(), "B") };
+        var activity = new BuildComponentActivity(Guid.NewGuid(), new DateTime(2022, 08, 07), new DateTime(2022, 08, 08));
+        var otherActivity = new BuildMachineActivity(Guid.NewGuid(), activity.FinishRestingDate.AddSeconds(1), activity.FinishRestingDate.AddMinutes(30));
+        var command = new ScheduleActivityCommand(otherActivity, workers.Select(x => x.Id).ToArray());
+
+        // Setup
+        workers.ForEach(worker => worker.WorksIn(activity));
+        unitOfWork.WorkerRepository.GetWorkersAsync(default).ReturnsForAnyArgs(workers);
+
+        // When
+        var result = await sut.HandleAsync(command);
+
+        // Then
+        result.IsSuccess.Should().BeTrue();
+        await unitOfWork.Received().ActivityRepository.CreateAsync(Arg.Is(command.Activity));
+        await unitOfWork.Received().WorkerRepository.UpdateAsync(Arg.Any<Guid>(), Arg.Any<Worker>());
+    }
+
     [Fact]
     public async Task Scheduling_Activity_Without_Overlaping_Should_Work()
     {
@@ -32,19 +57,20 @@ public class ScheduleActivityUseCaseTests
         result.Value.ActivityId.Should().Be(command.Activity.Id);
     }
 
-    [Fact]
-    public async Task Creating_Activity_That_Overlaps_Any_Worker_Should_Fail()
+    [Theory]
+    [MemberData(nameof(WorkerGenerator))]
+    public async Task Creating_Activity_That_Overlaps_Any_Worker_Should_Fail(List<Worker> workers, params int[] workersIndex)
     {
         // Given
         var sut = GetMockedUseCase();
-
-        var workers = new List<Worker>{ new(Guid.NewGuid(), "A"), new(Guid.NewGuid(), "B") };
+                
         var activity = new BuildComponentActivity(Guid.NewGuid(), new DateTime(2022, 08, 07), new DateTime(2022, 08, 08));
         var overlaping = new BuildMachineActivity(Guid.NewGuid(), new DateTime(2022, 08, 07, 10, 15, 00), new DateTime(2022, 08, 07, 11, 15, 00));
         var command = new ScheduleActivityCommand(overlaping, workers.Select(x => x.Id).ToArray());
         
         // Setup
-        workers.ForEach(worker => worker.WorksIn(activity));
+        foreach (var index in workersIndex) 
+            workers[index].WorksIn(activity);
         unitOfWork.WorkerRepository.GetWorkersAsync(default).ReturnsForAnyArgs(workers);
 
         // When
@@ -53,36 +79,11 @@ public class ScheduleActivityUseCaseTests
         // Then
         result.IsFailed.Should().BeTrue();
         result.Error.Should().Be(DomainErrors.OverlappingActivities);
+        await unitOfWork.WorkerRepository.DidNotReceiveWithAnyArgs().UpdateAsync(default, default);
+        await unitOfWork.ActivityRepository.DidNotReceiveWithAnyArgs().CreateAsync(default);
+        await unitOfWork.DidNotReceiveWithAnyArgs().SaveAsync();
     }
 
-    [Fact]
-    public async Task Creating_Activity_That_Workers_Can_Do_Should_Be_Success()
-    {
-        // Given
-        var sut = GetMockedUseCase();
-
-        var workers = new List<Worker>{ new(Guid.NewGuid(), "A"), new(Guid.NewGuid(), "B") };
-        var activity = new BuildComponentActivity(Guid.NewGuid(), new DateTime(2022, 08, 07), new DateTime(2022, 08, 08));
-        var otherActivity = new BuildMachineActivity(Guid.NewGuid(), activity.FinishRestingDate.AddSeconds(1), activity.FinishRestingDate.AddMinutes(30));
-        var command = new ScheduleActivityCommand(otherActivity, workers.Select(x => x.Id).ToArray());
-        
-        // Setup
-        workers.ForEach(worker => worker.WorksIn(activity));
-        unitOfWork.WorkerRepository.GetWorkersAsync(default).ReturnsForAnyArgs(workers);
-
-        // When
-        var result = await sut.HandleAsync(command);
-
-        // Then
-        result.IsSuccess.Should().BeTrue();
-        await unitOfWork.WorkerRepository.UpdateAsync(Arg.Any<Guid>(), Arg.Any<Worker>()).Received(2);
-    }
-
-    public static IEnumerable<object[]> OverlappingCommandsGenerator()
-    {
-        yield return new object[] { null };
-        yield return new object[] { new ScheduleActivityCommand(new BuildComponentActivity(Guid.NewGuid(), DateTime.Today.AddDays(-1), DateTime.Now.AddHours(1)), Guid.Empty) };
-    }
 
     [Theory]
     [MemberData(nameof(InvalidCommandGenerator))]
@@ -90,7 +91,6 @@ public class ScheduleActivityUseCaseTests
     {
         //Given
         var sut = GetMockedUseCase();
-        var worker = new Worker(Guid.NewGuid(), "B");
 
         // When
         var result = await sut.HandleAsync(invalidCommand);
@@ -106,6 +106,15 @@ public class ScheduleActivityUseCaseTests
         var sut = (IScheduleActivityUseCase)new ScheduleActivityUseCase(logger, unitOfWork);
 
         return sut;
+    }
+
+    public static IEnumerable<object[]> WorkerGenerator()
+    {
+        yield return new object[] { new List<Worker> { new(Guid.NewGuid(), "A") }, new[] { 0 } };
+        yield return new object[] { new List<Worker> { new(Guid.NewGuid(), "A"), new(Guid.NewGuid(), "B") }, new[] { 1 } };
+        yield return new object[] { new List<Worker> { new(Guid.NewGuid(), "A"), new(Guid.NewGuid(), "B"), new(Guid.NewGuid(), "C") }, new[] { 1, 2 } };
+        yield return new object[] { new List<Worker> { new(Guid.NewGuid(), "A"), new(Guid.NewGuid(), "B"), new(Guid.NewGuid(), "C") }, new[] { 0, 2 } };
+        yield return new object[] { new List<Worker> { new(Guid.NewGuid(), "A"), new(Guid.NewGuid(), "B"), new(Guid.NewGuid(), "C") }, new[] { 0 } };
     }
 
     public static IEnumerable<object[]> InvalidCommandGenerator()
