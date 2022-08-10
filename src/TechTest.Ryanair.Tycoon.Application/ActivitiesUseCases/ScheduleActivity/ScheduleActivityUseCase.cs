8791bfc2883e5d277267facb19d9ad7c93d1484d
Brenon Ortega <brenon.ortega@hotmail.com>
2022-08-10 07:48:00 -0300

    Implements rollback to activity assignment when failing.

--- src/TechTest.Ryanair.Tycoon.Application/ActivitiesUseCases/ScheduleActivity/ScheduleActivityUseCase.cs
+++ src/TechTest.Ryanair.Tycoon.Application/ActivitiesUseCases/ScheduleActivity/ScheduleActivityUseCase.cs
@@ -34,7 +34,6 @@ public class ScheduleActivityUseCase : IScheduleActivityUseCase, IUseCase<Schedu
 
     private async Task<Result<ScheduledActivityResponse>> ScheduleForWorkers(TimedActivity activity, IEnumerable<Worker> workers)
     {
-        var tasks = new List<Func<Task>>();
         foreach (var worker in workers)
         {
             var result = worker.WorksIn(activity);
@@ -47,19 +46,50 @@ public class ScheduleActivityUseCase : IScheduleActivityUseCase, IUseCase<Schedu
                 return Result.Fail<ScheduledActivityResponse>(result.Error);
             }
 
-            tasks.Add(() => _unitOfWork.WorkerRepository.UpdateAsync(worker.Id, worker));
+            await _unitOfWork.WorkerRepository.UpdateAsync(worker.Id, worker);
         }
 
-        await UpdateAll(activity, tasks);
+        var updateResult = await UpdateActivity(activity);
+
+        if (updateResult.IsFailed)
+            await RollbackWorkersAsync(workers, activity);
 
         return Result.Success(new ScheduledActivityResponse() { ActivityId = activity.Id });
     }
 
-    private async Task UpdateAll(TimedActivity activity, List<Func<Task>> tasks)
+    private async Task<Result> RollbackWorkersAsync(IEnumerable<Worker> workers, TimedActivity activity)
     {
-        tasks.Add(() => _unitOfWork.ActivityRepository.CreateAsync(activity));
-        tasks.Add(() => _unitOfWork.SaveAsync());
+        var tasks = new List<Task>(workers.Count());
+        foreach (var worker in workers)
+        {
+            var result = worker.Unassign(activity);
 
-        await Task.WhenAll(tasks.Select(x => x.Invoke()));
+            if (result.IsFailed)
+            {
+                _logger.LogError("Could not rollback assignment activity {id} from {startDate} - {endDate}.\n Worker {id} {name}.",
+                    activity.Id, activity.Start, activity.Finish, worker.Id, worker.Name, worker.ActualStatus);
+
+                return Result.Fail(result.Error);
+            }
+
+            tasks.Add(_unitOfWork.WorkerRepository.UpdateAsync(worker.Id, worker));
+        }
+
+        await Task.WhenAll(tasks);
+        var updated = await _unitOfWork.SaveAsync();
+        return updated;
+    }
+
+    private async Task<Result> UpdateActivity(TimedActivity activity)
+    {
+        var activityUpdated = await _unitOfWork.ActivityRepository.UpdateAsync(activity.Id, activity);
+        var updateResult = await _unitOfWork.SaveAsync();
+
+        if(activityUpdated.IsFailed || updateResult.IsFailed)
+        {
+            return Result.Fail(activityUpdated.Error ?? updateResult.Error);
+        }
+
+        return Result.Success();
     }
 }
--- tests/TechTest.Ryanair.Tycoon.IntegrationTests/Application/AssignExistentActivityUseCaseTests.cs
+++ tests/TechTest.Ryanair.Tycoon.IntegrationTests/Application/AssignExistentActivityUseCaseTests.cs
@@ -1,6 +1,7 @@
 ﻿using Awarean.Sdk.Result;
 using Microsoft.Extensions.DependencyInjection;
 using TechTest.Ryanair.Tycoon.Application.ActivitiesUseCases.AssignExistentActivity;
+using TechTest.Ryanair.Tycoon.Application.ActivitiesUseCases.CreateActivity;
 using TechTest.Ryanair.Tycoon.Application.Extensions;
 using TechTest.Ryanair.Tycoon.Application.WorkerUseCases.CreateWorker;
 using TechTest.Ryanair.Tycoon.Infra.Extensions;
@@ -16,8 +17,13 @@ namespace TechTest.Ryanair.Tycoon.IntegrationTests.Application
             var ids = await SeedWorkers(provider, 3);
             var sut = provider.GetRequiredService<IAssignExistentActivityUseCase>();
 
-            var command = new AssignExistentActivityCommand(activityId: Guid.NewGuid(), workers: ids);
+            var create = new CreateActivityCommand(Guid.NewGuid(), "Machine", new DateTime(2022, 03, 10), new DateTime(2022, 03, 11));
+                
+            await provider.GetRequiredService<ICreateActivityUseCase>().HandleAsync(create);
 
+            var command = new AssignExistentActivityCommand(activityId: create.Id, ids);
+
+            // ERROR IS THAT IT CREATES, SHOULD BE AN UPDATE - FIXED, NOW IT WILL BREAK TESTS.
             Result<AssignedActivityResponse> result = await sut.HandleAsync(command);
             result.IsSuccess.Should().BeTrue();
         }
@@ -28,7 +34,7 @@ namespace TechTest.Ryanair.Tycoon.IntegrationTests.Application
             for (var i = 0; i < count; i++)
             {
                 var command = new CreateWorkerCommand(Guid.NewGuid(), i.ToString());
-                await provider.GetRequiredService<ICreateWorkerUseCase>().HandleAsync(command);
+                var result = await provider.GetRequiredService<ICreateWorkerUseCase>().HandleAsync(command);
                 guids.Add(command.Id);
             }
 
