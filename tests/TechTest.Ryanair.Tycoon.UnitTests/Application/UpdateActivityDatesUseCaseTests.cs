2f7d69bdda0d9c4ba0fea3186a8347f962be8f11
Brenon Ortega <brenon.ortega@hotmail.com>
2022-08-11 09:21:12 -0300

    continued implementing use case tests

--- tests/TechTest.Ryanair.Tycoon.UnitTests/Application/UpdateActivityDatesUseCaseTests.cs
+++ tests/TechTest.Ryanair.Tycoon.UnitTests/Application/UpdateActivityDatesUseCaseTests.cs
@@ -39,17 +39,56 @@ public class UpdateActivityUseCaseTests
 
         var workerA = new Worker(Guid.NewGuid(), "A");
         var workerB = new Worker(Guid.NewGuid(), "B");
-        var activityA = new BuildMachineActivity(Guid.NewGuid(), new DateTime(2022, 10, 20, 10, 10, 00), new DateTime(2022, 10, 20, 10, 10, 00));
+        var activityA = new BuildMachineActivity(Guid.NewGuid(), new DateTime(2022, 10, 20, 10, 10, 00), new DateTime(2022, 10, 20, 10, 11, 00));
 
-        var toBeUpdatedActivity = new BuildComponentActivity(Guid.NewGuid(), activityA.FinishRestingDate, new DateTime(2022, 10, 10, 20, 15, 00));
+        var toBeUpdatedActivity = new BuildMachineActivity(Guid.NewGuid(), activityA.FinishRestingDate, activityA.FinishRestingDate.AddMinutes(1));
 
+        workerA.WorksIn(activityA).WorksIn(toBeUpdatedActivity);
+        workerB.WorksIn(activityA).WorksIn(toBeUpdatedActivity);
+        
+        unitOfWork.WorkerRepository.GetWorkersAsync(default).ReturnsForAnyArgs(new[] { workerA, workerB });
+        unitOfWork.ActivityRepository.GetAsync(Arg.Is(toBeUpdatedActivity.Id)).Returns(toBeUpdatedActivity);
+
+        var command = new UpdateActivityDatesCommand(
+            activity: toBeUpdatedActivity.Id, 
+            newStartDate: new DateTime(2022, 10, 11), 
+            newEndDate: new DateTime(2022, 10, 20, 10, 15, 00));
+    
+        // When
+        var result = await sut.HandleAsync(command);
+
+        // Then
+        result.IsSuccess.Should().BeTrue();
+        workerA.Activities.First().Start.Should().Be(command.NewStartDate);
+        workerB.Activities.First().Start.Should().Be(command.NewStartDate);
+        toBeUpdatedActivity.Start.Should().Be(command.NewStartDate);
+    }
+
+    [Fact]
+    public async void Updating_An_Component_Activity_Date_For_More_Than_One_Worker_Should_Be_Fail()
+    {
+        // Given
+        var logger = Substitute.For<ILogger<UpdateActivityDatesUseCase>>();
+        var unitOfWork = Substitute.For<IUnitOfWork>();
+        var sut = (IUpdateActivityDatesUseCase)new UpdateActivityDatesUseCase(logger, unitOfWork);
+
+        var workerA = new Worker(Guid.NewGuid(), "A");
+        var workerB = new Worker(Guid.NewGuid(), "B");
+        var activityA = new BuildMachineActivity(Guid.NewGuid(), new DateTime(2022, 10, 20, 10, 10, 00), new DateTime(2022, 10, 20, 10, 11, 00));
+
+        var toBeUpdatedActivity = new BuildComponentActivity(Guid.NewGuid(), activityA.FinishRestingDate, activityA.FinishRestingDate.AddMinutes(1));
 
         workerA.WorksIn(activityA).WorksIn(toBeUpdatedActivity);
         workerB.WorksIn(activityA).WorksIn(toBeUpdatedActivity);
 
+        unitOfWork.WorkerRepository.GetWorkersAsync(Arg.Is(toBeUpdatedActivity.Workers)).Returns(new[] { workerA, workerB });
+        unitOfWork.ActivityRepository.GetAsync(Arg.Is(toBeUpdatedActivity.Id)).Returns(toBeUpdatedActivity);
 
-        var command = new UpdateActivityDatesCommand(activity: toBeUpdatedActivity.Id, newStartDate: new DateTime(2022, 10, 11), newEndDate: new DateTime(2022, 10, 20, 10, 15, 00));
-    
+        var command = new UpdateActivityDatesCommand(
+            activity: toBeUpdatedActivity.Id,
+            newStartDate: new DateTime(2022, 10, 11),
+            newEndDate: new DateTime(2022, 10, 20, 10, 15, 00));
+
         // When
         var result = await sut.HandleAsync(command);
 
